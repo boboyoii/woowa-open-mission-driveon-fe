0be84e8c977cbbe4f58cd62cd9569bdb5a564c6b
boyeon <boyeon6331@gamil.com>
2025-11-10 22:35:52 +0900

    style: 게임 시작 화면의 배경과 버튼 디자인 수정

--- src/scenes/StartScene.js
+++ src/scenes/StartScene.js
@@ -7,8 +7,6 @@ export default class StartScene extends Phaser.Scene {
 
   preload() {
     this.load.image('start_bg', 'assets/start_bg.jpg');
-    this.load.image('input_box', 'assets/input_box.jpg');
-    this.load.image('btn_start', 'assets/btn_start.jpg');
   }
 
   create() {
@@ -17,9 +15,6 @@ export default class StartScene extends Phaser.Scene {
     this.bg = this.add.image(W / 2, H / 2, 'start_bg');
     this.bg.setDisplaySize(W, H);
 
-    this.inputBox = this.add.image(W / 2, H * 0.55, 'input_box');
-    this.inputBox.setScale(0.25);
-
     this.nameInput = this.add.dom(W / 2.15, H * 0.54, 'input');
 
     const el = this.nameInput.node;
@@ -27,11 +22,30 @@ export default class StartScene extends Phaser.Scene {
     el.setAttribute('placeholder', '닉네임 입력');
     el.classList.add('nickname-input');
 
-    const btn = this.add.image(W / 2, H * 0.75, 'btn_start');
-    btn.setScale(0.7);
-    btn.setInteractive({ useHandCursor: true });
+    const makeBtn = (x, y, label, onClick) => {
+      const btn = this.add
+        .rectangle(x, y, 180, 52, 0xffffff, 1)
+        .setDepth(10)
+        .setInteractive({ useHandCursor: true });
+      const txt = this.add
+        .text(x, y, label, {
+          fontSize: '20px',
+          color: '#222',
+          fontFamily: 'Pretendard, sans-serif',
+        })
+        .setOrigin(0.5)
+        .setDepth(11);
 
-    btn.on('pointerdown', () => {
+      btn.on('pointerover', () => btn.setScale(1.05));
+      btn.on('pointerout', () => btn.setScale(1.0));
+      btn.on('pointerdown', () => {
+        btn.setScale(0.98);
+        onClick();
+      });
+      return { btn, txt };
+    };
+
+    makeBtn(W / 2, H * 0.75, 'START', () => {
       const playerName = this.nameInput.node.value.trim();
       if (!playerName) {
         alert('이름을 입력해주세요!');
@@ -40,10 +54,5 @@ export default class StartScene extends Phaser.Scene {
 
       this.scene.start('GameScene', { playerName });
     });
-
-    btn.on('pointerover', () => btn.setScale(0.8));
-    btn.on('pointerout', () => btn.setScale(0.7));
-    btn.on('pointerdown', () => btn.setScale(0.65));
-    btn.on('pointerup', () => btn.setScale(0.8));
   }
 }
