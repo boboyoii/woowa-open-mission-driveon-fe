a70f569561f0745df97a4043062cf505c0b00123
boyeon <boyeon6331@gamil.com>
2025-11-10 15:53:15 +0900

    feat: 기본 배경 및 자동차 이동 기능 추가 - 도로 배경 이미지를 연속적으로 전환해 움직이는 효과 구현 - ←, → 키 입력으로 자동차를 좌우로 이동 가능하도록 구현

---
+++ src/scenes/GameScene.js
@@ -0,0 +1,42 @@
+export default class GameScene extends Phaser.Scene {
+  constructor() {
+    super('GameScene');
+  }
+
+  preload() {
+    for (let i = 1; i <= 23; i++) {
+      this.load.image(`road_${i}`, `assets/road/road_${i}.jpg`);
+    }
+    this.load.image('car', 'assets/car.png');
+  }
+
+  create() {
+    const { width: W, height: H } = this.scale;
+
+    this.totalRoads = 23;
+    this.currentRoadIndex = 1;
+    this.bg = this.add.image(W / 2, H / 2, 'road_1');
+
+    this.bg.setDisplaySize(W, H).setDepth(-10);
+
+    this.time.addEvent({
+      delay: 100,
+      loop: true,
+      callback: () => {
+        this.currentRoadIndex++;
+        if (this.currentRoadIndex > this.totalRoads) this.currentRoadIndex = 1;
+        this.bg.setTexture(`road_${this.currentRoadIndex}`);
+      },
+    });
+
+    this.car = this.add.image(W / 2, H * 0.85, 'car');
+    this.car.setScale(0.5);
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+  }
+
+  update() {
+    if (this.cursors.left.isDown) this.car.x -= 5;
+    if (this.cursors.right.isDown) this.car.x += 5;
+  }
+}
